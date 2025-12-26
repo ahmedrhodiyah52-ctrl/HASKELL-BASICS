5215417a9adf116d2351af0413c08af0eb3067f3
ahmedrhodiyah52-ctrl <ahmedrhodiyah52@gmail.com>
2025-12-26 18:25:17 +0100

    Update Chapter 1.hs

--- Practical Tasks/Chapter 1.hs
+++ Practical Tasks/Chapter 1.hs
@@ -1,6 +1,11 @@
-add :: Int -> Int 
-add x = x * 2
+--HC1T1 - Task 1: Function Composition
+double: Multiplies a number by 2.
+
+
+
+double :: Int -> Int 
+double x = x * 2
 
 main :: IO () 
 main = do 
- print $ add 5
+ print $ double 5 
